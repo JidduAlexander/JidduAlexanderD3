6ba8ce19ec9287b35afb8c01ee5a533cf48f9561
Jiddu Alexandergit config --global user.email san@nesware.net <you@example.com>
2024-06-14 21:04:46 +0200

    get app update

--- docs/page-get-app.js
+++ docs/page-get-app.js
@@ -1,8 +1,8 @@
 
-const svg = d3.select('#svg-get')
+const svgGet = d3.select('#svg-get')
   .attr("viewBox","0 0 100 945")
   .attr('margin', "auto");
 
-const getGhg = new GetGhg(svg, dataGetGhg, {id: "main", settings: dataSettings})
+const getGhg = new GetGhg(svgGet, dataGetGhg, {id: "main", settings: dataSettings})
 
 
